265d1eb3fd1d5c95298db2b2d8720b4af1921efb
Dustin Schau <dustinschau@gmail.com>
2018-12-19 16:32:37 -0600

    feat: initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# blog (1.0.0)
+
+The source code for the [Gatsby][gatsby] driven statically generated blog
+
+## Tech stack
+
+- [gatsby v2][gatsby]
+  - An awesome React-driven static site generator
+- CSS in JS with [emotion][emotion]
+  - "The Next Generation of CSS-in-JS," used as a styling solution
+- [particles.js][particles.js]
+  - A cool particle effect seen in the header
+- [jest][jest]
+  - Unit test runner with first class snapshot support, watch mode, etc.
+- [markdown][markdown]
+  - Each and every post is authored in Markdown parsed by [remark][remark]
+
+[gatsby]: https://github.com/gatsbyjs/gatsby
+[emotion]: https://emotion.sh/
+[particles.js]: https://github.com/VincentGarreau/particles.js/
+[markdown]: https://en.wikipedia.org/wiki/Markdown
+[remark]: http://remark.js.org/
+[jest]: https://facebook.github.io/jest/
---
+++ gatsby-node.js
@@ -0,0 +1,133 @@
+const path = require('path');
+const { GraphQLString } = require('gatsby/graphql');
+const slugify = require('limax');
+const path = require('path');
+
+const createTagPages = (createPage, edges) => {
+  const tagTemplate = path.resolve(`src/templates/tags.js`);
+  const posts = {};
+
+  edges
+    .forEach(({ node }) => {
+      if (node.frontmatter.tags) {
+        node.frontmatter.tags
+          .forEach(tag => {
+            if (!posts[tag]) {
+              posts[tag] = [];
+            }
+            posts[tag].push(node);
+          });
+      }
+    });
+
+  const tags = Object.keys(posts);
+  
+  createPage({
+    path: '/tags',
+    component: tagTemplate,
+    context: {
+      tags
+    }
+  });
+
+  Object.keys(posts)
+    .forEach(tagName => {
+      const tag = posts[tagName];
+      createPage({
+        path: `/tags/${tagName}`,
+        component: tagTemplate,
+        context: {
+          tags,
+          tag,
+          tagName
+        }
+      })
+    });
+};
+
+exports.createPages = function createPages({ actions, graphql }) {
+  const { createPage } = actions;
+
+  const blogPostTemplate = path.resolve(`src/templates/blog-post.js`);
+
+  const draftFilter = `
+    filter: {
+      frontmatter: { draft: { ne: true }}
+    }
+  `;
+
+  return graphql(`{
+    allMarkdownRemark(
+      sort: { order: ASC, fields: [frontmatter___date] }
+      ${process.env.NODE_ENV === 'production' ? draftFilter : ''}
+      limit: 1000
+    ) {
+      edges {
+        node {
+          excerpt(pruneLength: 250)
+          html
+          id
+          timeToRead
+          slug
+          frontmatter {
+            date(formatString: "MMMM DD, YYYY")
+            draft
+            tags
+            title
+          }
+        }
+      }
+    }
+  }`)
+    .then(result => {
+      if (result.errors) {
+        return Promise.reject(result.errors)
+      }
+
+      const posts = result.data.allMarkdownRemark.edges;
+
+      createTagPages(createPage, posts);
+
+      // Create pages for each markdown file.
+      posts.forEach(({ node }, index) => {
+        const { slug } = node;
+        createPage({
+          path: slug,
+          component: blogPostTemplate,
+          context: {
+            prev: index === 0 ? null : posts[index - 1].node,
+            next: index === posts.length - 1 ? null : posts[index + 1].node,
+            slug
+          }
+        });
+      });
+
+      return posts;
+    });
+};
+
+
+exports.setFieldsOnGraphQLNodeType = function setFieldsOnGraphQLNode({ type }) {
+  switch (type.name) {
+    case 'MarkdownRemark':
+      return {
+        type: {
+          type: GraphQLString,
+          resolve(source) {
+            const { fileAbsolutePath } = source;
+            const [,folder] = fileAbsolutePath.split(path.resolve('content')).pop().split('/');
+            return folder;
+          }
+        },
+        slug: {
+          type: GraphQLString,
+          resolve(source) {
+            const { frontmatter } = source;
+            return frontmatter.path || frontmatter.slug || `/${slugify(frontmatter.title)}`;
+          }
+        }
+      }
+    default:
+      return {};
+  }
+};
