4d6aaeb26517a9d808149d79da0d5cf1a9f80616
Dustin Schau <dustinschau@gmail.com>
2018-12-27 11:00:38 -0600

    chore: export a function and use root arg

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,68 +1,75 @@
-module.exports = {
-  siteMetadata: {
-    title: 'Dustin Schau - Blog',
-    author: 'Dustin Schau',
-  },
-  plugins: [
-    {
-      resolve: 'gatsby-plugin-manifest',
-      options: {
-        name: `Dustin Schau's Blog`,
-        description: 'The blog of the developer, Dustin Schau',
-        short_name: 'DSchau Blog',
-        background_color: 'white',
-        theme_color: '#002635',
-        orientation: 'portrait',
-        display: 'minimal-ui',
-      },
+const path = require('path')
+
+module.exports = function themeConfig({
+  blogContent = path.join('content', 'blog'),
+  root
+} = {}) {
+  return {
+    siteMetadata: {
+      title: 'Dustin Schau - Blog',
+      author: 'Dustin Schau',
     },
-    'gatsby-plugin-catch-links',
-    'gatsby-plugin-emotion',
-    'gatsby-plugin-remove-trailing-slashes',
-    'gatsby-plugin-twitter',
-    {
-      resolve: 'gatsby-source-filesystem',
-      options: {
-        path: `${__dirname}/content/blog`,
-        name: 'post',
+    plugins: [
+      {
+        resolve: 'gatsby-plugin-manifest',
+        options: {
+          name: `Dustin Schau's Blog`,
+          description: 'The blog of the developer, Dustin Schau',
+          short_name: 'DSchau Blog',
+          background_color: 'white',
+          theme_color: '#002635',
+          orientation: 'portrait',
+          display: 'minimal-ui',
+        },
       },
-    },
-    {
-      resolve: `gatsby-transformer-remark`,
-      options: {
-        plugins: [
-          'gatsby-remark-copy-linked-files',
-          {
-            resolve: 'gatsby-remark-images',
-            options: {
-              backgroundColor: 'transparent',
-              linkImagesToOriginal: false,
-              showCaptions: true,
+      'gatsby-plugin-catch-links',
+      'gatsby-plugin-emotion',
+      'gatsby-plugin-remove-trailing-slashes',
+      'gatsby-plugin-twitter',
+      {
+        resolve: 'gatsby-source-filesystem',
+        options: {
+          path: path.join(root, blogContent),
+          name: 'post',
+        },
+      },
+      {
+        resolve: `gatsby-transformer-remark`,
+        options: {
+          plugins: [
+            'gatsby-remark-copy-linked-files',
+            {
+              resolve: 'gatsby-remark-images',
+              options: {
+                backgroundColor: 'transparent',
+                linkImagesToOriginal: false,
+                showCaptions: true,
+              },
             },
-          },
-          'gatsby-remark-prismjs',
-          'gatsby-remark-smartypants',
-          'gatsby-remark-autolink-headers',
-        ],
+            'gatsby-remark-prismjs',
+            'gatsby-remark-smartypants',
+            'gatsby-remark-autolink-headers',
+          ],
+        },
       },
-    },
-    'gatsby-plugin-react-helmet',
-    'gatsby-plugin-layout',
-    'gatsby-transformer-sharp',
-    'gatsby-plugin-sharp',
-    'gatsby-plugin-offline',
-    {
-      resolve: 'gatsby-plugin-typography',
-      options: {
-        omitGoogleFont: true,
-        pathToConfigModule: 'src/utils/typography',
+      'gatsby-plugin-react-helmet',
+      'gatsby-plugin-layout',
+      'gatsby-transformer-sharp',
+      'gatsby-plugin-sharp',
+      'gatsby-plugin-offline',
+      {
+        resolve: 'gatsby-plugin-typography',
+        options: {
+          omitGoogleFont: true,
+          pathToConfigModule: path.join(root, 'src', 'utils', 'typography'),
+        },
       },
-    },
-    {
-      resolve: 'gatsby-plugin-google-analytics',
-      options: {
-        trackingId: 'UA-102928446-2',
+      {
+        resolve: 'gatsby-plugin-google-analytics',
+        options: {
+          trackingId: 'UA-102928446-2',
+        },
       },
-    },
-  ],
+    ],
+  }
 };
